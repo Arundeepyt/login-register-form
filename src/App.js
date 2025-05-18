71e3fd5391690d9f392577626e93283509cf661c
arundeepyt <arunsodhi16@gmail.com>
2025-05-18 13:59:58 +0530

    intlise

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Form from "./Form";
+import "./App.css";
 
 function App() {
+  const [isRegistered, setIsRegistered] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{isRegistered ? "Welcome back!" : "Create an account"}</h1>
+
+      <Form isRegistered={isRegistered} />
+
+      <p style={{ marginTop: 12 }}>
+        {isRegistered ? "Need an account?" : "Already registered?"}{" "}
+        <button type="button" onClick={() => setIsRegistered((p) => !p)}>
+          {isRegistered ? "Register" : "Login"} instead
+        </button>
+      </p>
     </div>
   );
 }
